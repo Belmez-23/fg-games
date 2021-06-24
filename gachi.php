976e883bc2f7aefb7978e99957dc463bd063d500
Belmez-23 <59193727+Belmez-23@users.noreply.github.com>
2021-06-24 15:21:43 +0300

    Add files via upload

---
+++ gachi.php
@@ -0,0 +1,18 @@
+<?php
+
+$ishod = file('data.txt');
+//$ishod = $_POST;
+$s = explode(' ', $ishod[0]);
+$ss = count($s);
+$gachi = array('300 bucks', 'Ass', 'boy', 'Fuck', 'Fuck you', 'leather man', 'door', 'Deep dark fantasies', 'Dungeon master', 'Cumming', 'slave', 'Stick your finger', 'amazing');
+$gg = count($gachi);
+foreach ($gachi as $g) {
+    $rs = rand(0, $ss);
+    $rg = rand(0, $gg);
+    $s[$rs] = $g;
+}
+foreach ($s as $ass) {
+    echo $ass . " ";
+}
+
+?>
